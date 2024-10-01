04706501af693725a6637c8aa13381adc7c81821
Amanda Mokhachane <AMokhachane@singular.co.za>
2024-10-01 16:59:12 +0200

    Login up to date

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import axios from "axios"; 
-import styles from "./Login.module.css"; 
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAppleAlt } from '@fortawesome/free-solid-svg-icons'; 
+import axios from "axios";
+import styles from "./Login.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAppleAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
   const [successMessage, setSuccessMessage] = useState("");
-  const [serverError, setServerError] = useState(""); // To handle server-side errors
+  const [serverError, setServerError] = useState("");
   const navigate = useNavigate();
 
   const validateForm = () => {
@@ -25,16 +26,20 @@ const Login = () => {
     e.preventDefault();
     if (validateForm()) {
       try {
-        // API call to login
-        const response = await axios.post("http://localhost:5264/api/accounts/login", {
-          email: email, 
-          password: password,
-        });
+        const response = await axios.post(
+          "http://localhost:5264/api/accounts/login",
+          {
+            email: email,
+            password: password,
+          }
+        );
 
-        // Handle success
         if (response.status === 200) {
           const { userEmail, userName, userID } = response.data;
-          localStorage.setItem("user", JSON.stringify({ userName, userEmail, userID }));
+          localStorage.setItem(
+            "user",
+            JSON.stringify({ userName, userEmail, userID })
+          );
           setSuccessMessage("Login successful!");
           setEmail("");
           setPassword("");
@@ -42,7 +47,6 @@ const Login = () => {
           navigate("/home");
         }
       } catch (error) {
-        // Handle errors from the server
         if (error.response) {
           setServerError(error.response.data.message);
         } else {
@@ -57,19 +61,23 @@ const Login = () => {
       <div className={styles.left}>
         <div className={styles.logo}>
           <div className={styles.logoSquare}>
-            <FontAwesomeIcon icon={faAppleAlt} size="3x" color="#green" />
+            <FontAwesomeIcon icon={faAppleAlt} size="3x" color="green" />
           </div>
           <span className={styles.boldText}>FRESH FRUITS & VEGGIES</span>
         </div>
-        {successMessage && <p className={styles.successMessage}>{successMessage}</p>}
-        {serverError && <p className={styles.errorMessage}>{serverError}</p>} {/* Display server errors */}
+        {successMessage && (
+          <p className={styles.successMessage}>{successMessage}</p>
+        )}
+        {serverError && <p className={styles.errorMessage}>{serverError}</p>}
+
+        {/* Form needs to be inside the div */}
         <form onSubmit={handleLogin}>
           <div className={styles.Group}>
-            <label className={styles.label}>Email</label>
-            {errors.email && <p className={styles.errorMessage}>{errors.email}</p>}
+            <FontAwesomeIcon icon={faUser} className={styles.icon} />
             <input
               type="email"
               className={styles.inputField}
+              placeholder="Username"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -77,17 +85,18 @@ const Login = () => {
           </div>
 
           <div className={styles.Group}>
-            <label className={styles.label}>Password</label>
-            {errors.password && <p className={styles.errorMessage}>{errors.password}</p>}
+            <FontAwesomeIcon icon={faLock} className={styles.icon} />
             <input
               type="password"
               className={styles.inputField}
+              placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
 
+          {/* Forgot Password and Login button inside the form */}
           <div className={styles.forgotPasswordAndButton}>
             <Link to="/email" className={styles.forgotPassword}>
               Forgot Password?
@@ -96,17 +105,23 @@ const Login = () => {
               Login
             </button>
           </div>
-        </form>
-        
-        {/* Register link section */}
-        <div className={styles.registerLink}>
-          <p>If you don't have an account, <Link to="/register" className={styles.register}>register here</Link>.</p>
-        </div>
+        </form> {/* Closing form here */}
+
       </div>
 
       <div className={styles.rightSide}>
-        <div className={styles.iconGrid}>
-          {/* <img src="https://res.cloudinary.com/drgxphf5l/image/upload/v1726736758/qwbddlqxjjgxsvbdcudg.png" className='image' /> */}
+        <div className={styles.welcomeSection}>
+          <h1 className={styles.welcomeText}>Hello and welcome!</h1>
+          <p className={styles.welcomeSubtext}>
+		  If you're new, take a moment to create an account and unlock all the amazing features we have to offer.
+          </p>
+          <p>
+            Click{" "}
+            <Link to="/register" className={styles.signUpLink}>
+              HERE
+            </Link>
+			to register{" "}
+          </p>
         </div>
       </div>
     </div>
